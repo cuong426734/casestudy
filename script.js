db148a7a7a3ea5294e71040d25835bb02a7be069
Trần Cường <cuong@MacBook-Pro-cua-Tran.local>
2022-08-01 01:07:49 +0700

    snake

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ const ctx = canvas.getContext('2d')
 const BACKGROUND_COLOR  = 'black'
 ctx.fillStyle = BACKGROUND_COLOR
 ctx.fillRect(0, 0, game_width, game_width)
+const game_delay = 100
 
 const left= 37
 const up = 38
 const right = 39
 const down = 40
+
 
 
 class Vector2d{
@@ -20,6 +22,10 @@ class Vector2d{
         this.y = y
     }
 }
+
+let currenDirection = new Vector2d(-1, 0)
+
+
 class Snake {
     constructor() {
         this.body = [
@@ -27,6 +33,7 @@ class Snake {
             new Vector2d(UNIT * 7, UNIT * 3),
             new Vector2d(UNIT * 8, UNIT * 3),
         ]
+        this.head = this.body[0]
         this.speed = new Vector2d(-1, 0)
     }
 
@@ -47,6 +54,23 @@ class Snake {
             ctx.fillRect(this.body[i].x, this.body[i].y, UNIT, UNIT)
         }
     }
+
+    handleBound(){
+        if(this.head.x <0){
+            this.head.x = game_width -UNIT
+        }
+        if(this.head.x > game_width -UNIT){
+          this.head.x = 0
+        }
+
+        if(this.head.y <0){
+            this.head.y = game_width -UNIT
+        }
+        if(this.head.y > game_width -UNIT){
+            this.head.y = 0
+        }
+    }
+
     move(){
         this.clear()
         for (let i = this.body.length -1; i>=1 ; i--) {
@@ -56,36 +80,97 @@ class Snake {
 
         this.body[0].x += this.speed.x * UNIT
         this.body[0].y += this.speed.y * UNIT
+        this.handleBound()
 
+        this.draw()
+    }
+
+    checkEat(food){
+        let head = this.body[0]
+        return food.x === head.x && food.y === head.y
+    }
+
+    grow(){
+        this.clear()
+        let snakeLength = this.body.length
+        let mountX = this.body[snakeLength -1].x - this.body[snakeLength - 2].x
+        let mountY = this.body[snakeLength -1].y - this.body[snakeLength - 2].y
+
+        let newPart = new Vector2d(
+            this.body[snakeLength - 1].x + mountX,
+            this.body[snakeLength - 1].y + mountY,
+        )
+        this.body.push(newPart)
+        this.draw()
+
+    }
+}
+
+class Food{
+    constructor(x, y){
+        this.x = x
+        this.y = y
+    }
+
+    draw(){
+        ctx.fillStyle = 'green'
+        ctx.fillRect(this.x, this.y, UNIT, UNIT)
+    }
+
+    clear(){
+        ctx.fillStyle = BACKGROUND_COLOR
+        ctx.fillRect(this.x, this.y, UNIT, UNIT)
+    }
+
+    getRandomNumber(){
+        let randomNumber = Math.floor(Math.random() * game_width)
+        randomNumber -= randomNumber % UNIT
+        return randomNumber
+    }
+    spawn (){
+        this.clear()
+        this.x = this.getRandomNumber()
+        this.y = this.getRandomNumber()
         this.draw()
     }
 }
 
 let player = new Snake()
 player.draw()
+let food = new Food()
+food.spawn()
 
 setInterval(() => {
     player.move()
-},200);
+    if (player.checkEat(food)){
+        food.spawn()
+    }
+},game_delay);
 
 document.onkeydown = function(e){
     switch(e.keyCode){
         case left:
+            if(currenDirection.x === 1) break
             player.speed = new Vector2d(-1, 0)
+            currenDirection = new Vector2d(-1, 0)
             break;
         case right:
+            if(currenDirection.x === -1) break
             player.speed = new Vector2d(1, 0)
+            currenDirection = new Vector2d(1, 0)
             break;
         case up:
+            if(currenDirection.y === 1) break
             player.speed = new Vector2d(0, -1)
+            currenDirection = new Vector2d(0, -1)
             break;
         case down:
+            if(currenDirection.y === -1) break
             player.speed = new Vector2d(0, 1)
+            currenDirection = new Vector2d(0, 1)
             break;
 
         default:
             break;
     }
 }
-
-
